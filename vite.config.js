800763d09cc8cda396f134edb3d16bf318a81bbb
Eduardo Fleitas <fleitaseduardo141@gmail.com>
2022-11-06 20:21:42 -0300

    "Test"

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Menu-crud-tarea/',
+  base: '/menu-tarea/',
   plugins: [vue()],
   resolve: {
     alias: {
